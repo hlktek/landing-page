8b2f0f4e5b447120a33c425e47f00750d54e7a3d
Chinh <chinh.ngo@gemi.io>
2021-01-29 17:13:39 +0700

    fi

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -26,14 +26,27 @@ $(document).ready(function () {
   $("#slide-banner").carousel();
 
   $("#logout").on("click", function (e) {
+    clearCookies('mysession');
+  });
+  
+  setInterval(function(){
+    console.log("Hello"); 
+    clearCookies('mysession');
+  }, 10800000); // 3 hours = 10800000 miliseconds
+
+  function clearCookies(cookieName) {
     var cookies = document.cookie.split(";");
 
     for (var i = 0; i < cookies.length; i++) {
       var cookie = cookies[i];
       var eqPos = cookie.indexOf("=");
       var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
-      document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+      
+      if(name == cookieName){
+        document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+        window.location.reload();
+      }
     }
-    window.location.reload();
-  });
+  }
+
 });
