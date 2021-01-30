96517af09a9c035827ea7a21739d90bfc2db70f8
Chinh <chinh.ngo@gemi.io>
2021-01-30 11:30:42 +0700

    fix clear cookie

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -42,7 +42,7 @@ $(document).ready(function () {
       var eqPos = cookie.indexOf("=");
       var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
       
-      if(name == cookieName){
+      if(name.trim() == cookieName){
         document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
         window.location.reload();
       }
